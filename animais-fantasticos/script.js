5b2056994e9bd2fe88a4661e979c943fb226ada4
Gustavo Martins <gustavo.martins@idwall.co>
2024-10-22 19:51:01 -0300

    0311 smooth scroll

--- animais-fantasticos/script.js
+++ animais-fantasticos/script.js
@@ -44,3 +44,29 @@ function initAccordion() {
 
 initAccordion();
 
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+    const top = section.offsetTop;
+
+    // window.scrollTo({
+    //   top: top,
+    //   behavior:'smooth',
+    // })
+
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    })
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
+  });
+}
+
+initScrollSuave();
