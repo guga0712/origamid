9cbd1d839947f853db500f872baef8801b31fb05
guga0712 <martins0712@hotmail.com>
2024-10-22 21:14:47 -0300

    0401 constructor exercise

--- objetos/script.js
+++ objetos/script.js
@@ -1,6 +1,74 @@
-function Carro(marca, preco) {
-  this.marca = marca;
-  this.preco = preco
+// const Dom = {
+//   seletor: 'li',
+//   element() {
+//     return document.querySelector(this.seletor)
+//   },
+//   ativar() {
+//     this.element().classList.add('ativar')
+//   }
+// }
+
+// function Dom(seletor) {
+//   this.element = function () {
+//     return document.querySelector(seletor)
+
+//   }
+//   this.ativar = function (classe) {
+//     this.element().classList.add(classe)
+//   }
+// }
+
+// const li = new Dom('li');
+// li.ativar('ativar');
+
+
+// Transforme o objeto abaixo em uma Constructor Function
+const pessoa = {
+  nome: 'Nome pessoa',
+  idade: 0,
+  andar() {
+    console.log(this.nome + ' andou');
+  }
 }
 
-const honda = new Carro('honda', 200)
+function pessoaConstructor(nome, idade) {
+  this.nome = nome;
+  this.idade = idade;
+  this.andar = function () {
+    console.log(this.nome + ' andou');
+  }
+}
+
+
+// Crie 3 pessoas, João - 20 anos,
+// Maria - 25 anos, Bruno - 15 anos
+
+const joao = new pessoaConstructor('João', 20);
+const maria = new pessoaConstructor('Maria', 25);
+const bruno = new pessoaConstructor('Bruno', 15);
+
+
+
+// Crie uma Constructor Function (Dom) para manipulação
+// de listas de elementos do dom. Deve conter as seguintes
+// propriedades e métodos:
+//
+// elements, retorna NodeList com os elementos selecionados
+// addClass(classe), adiciona a classe a todos os elementos
+// removeClass(classe), remove a classe a todos os elementos
+
+function Dom(selector) {
+  this.elements = function () {
+    return document.querySelectorAll(selector)
+  }
+  this.addClass = function (classe) {
+    this.elements().forEach(element => {
+      element.classList.add(classe)
+    })
+  }
+  this.removeClass = function (classe) {
+    this.elements().forEach(element => {
+      element.classList.remove(classe)
+    })
+  }
+}
