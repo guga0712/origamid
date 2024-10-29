94609cdedc844f97419ecabf6e15f3dbb2241f5b
guga0712 <martins0712@hotmail.com>
2024-10-29 19:36:24 -0300

    TS 0201 Annotation e inference

--- TypeScript/types/script.js
+++ TypeScript/types/script.js
@@ -1,5 +1,21 @@
 "use strict";
+const produto = "Livro";
+const preco = 200;
+const carro = {
+    marca: "Audi",
+    portas: 5,
+};
+const barato = preco < 400 ? true : 'produto caro';
 function somar(a, b) {
     return a + b;
 }
-console.log(somar(5, 20));
+const nintendo = {
+    nome: 'Nintendo',
+    preco: '2000'
+};
+function transformarPreco(produto) {
+    produto.preco = "R$ " + produto.preco;
+    return produto;
+}
+const produtoNovo = transformarPreco(nintendo);
+console.log(produtoNovo);
--- TypeScript/types/script.ts
+++ TypeScript/types/script.ts
@@ -1,5 +1,27 @@
+const produto = "Livro"
+const preco = 200;
+
+const carro = {
+  marca: "Audi",
+  portas: 5,
+}
+
+const barato = preco < 400 ? true : 'produto caro';
+
 function somar(a: number, b: number) {
   return a + b;
 }
 
-console.log(somar(5, 20))
+const nintendo = {
+  nome: 'Nintendo',
+  preco: '2000'
+}
+
+function transformarPreco(produto: { nome: string; preco: string }) {
+  produto.preco = "R$ " + produto.preco;
+  return produto;
+}
+
+const produtoNovo = transformarPreco(nintendo)
+
+console.log(produtoNovo)
