6511d0e75531504e0484fad7a2f8cdd132577980
guga0712 <martins0712@hotmail.com>
2024-10-29 19:53:27 -0300

    TS 0201 Exercise

--- TypeScript/types/script.js
+++ TypeScript/types/script.js
@@ -1,21 +1,5 @@
 "use strict";
-const produto = "Livro";
-const preco = 200;
-const carro = {
-    marca: "Audi",
-    portas: 5,
-};
-const barato = preco < 400 ? true : 'produto caro';
-function somar(a, b) {
-    return a + b;
+function normalizarTexto(texto) {
+    return texto.trim().toLowerCase();
 }
-const nintendo = {
-    nome: 'Nintendo',
-    preco: '2000'
-};
-function transformarPreco(produto) {
-    produto.preco = "R$ " + produto.preco;
-    return produto;
-}
-const produtoNovo = transformarPreco(nintendo);
-console.log(produtoNovo);
+console.log(normalizarTexto(' Caralh '));
--- TypeScript/types/script.ts
+++ TypeScript/types/script.ts
@@ -1,27 +1,5 @@
-const produto = "Livro"
-const preco = 200;
-
-const carro = {
-  marca: "Audi",
-  portas: 5,
+function normalizarTexto(texto: string) {
+  return texto.trim().toLowerCase();
 }
 
-const barato = preco < 400 ? true : 'produto caro';
-
-function somar(a: number, b: number) {
-  return a + b;
-}
-
-const nintendo = {
-  nome: 'Nintendo',
-  preco: '2000'
-}
-
-function transformarPreco(produto: { nome: string; preco: string }) {
-  produto.preco = "R$ " + produto.preco;
-  return produto;
-}
-
-const produtoNovo = transformarPreco(nintendo)
-
-console.log(produtoNovo)
+console.log(normalizarTexto(' Caralh '))
