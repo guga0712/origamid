33383708b6324943303483bf24497fe254a06418
guga0712 <martins0712@hotmail.com>
2024-10-29 20:30:50 -0300

    TS 0204 - types

---
+++ TypeScript/interfaces/script.ts
@@ -0,0 +1,30 @@
+"use strict";
+
+type Produto = {
+  nome: string;
+  preco: number;
+  teclado: boolean;
+}
+
+function preencherDados(dados: Produto) {
+  document.body.innerHTML += `
+  <div>
+    <h2>${dados.nome}</h2>
+    <p>R$ ${dados.preco}</p>
+    <p>Teclado: ${dados.teclado ? 'Sim' : 'Não'}</p>
+  </div>
+  `;
+}
+preencherDados({
+  nome: 'Computador',
+  preco: 2000,
+  teclado: true
+});
+
+preencherDados({
+  nome: 'Notebook',
+  preco: 2500,
+  teclado: true
+});
+
+
